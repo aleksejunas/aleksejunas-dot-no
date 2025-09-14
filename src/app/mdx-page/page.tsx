7cd5aaa7c756de5bce36e936e91b619f7743252a
aleksejunas <photo@seventyseven.no>
2025-09-14 17:34:48 +0200

    added overrideComponents functionality
    - added a way to override styling over the globally set markdown styling

--- src/app/mdx-page/page.tsx
+++ src/app/mdx-page/page.tsx
@@ -1,5 +1,18 @@
 import Welcome from "@/app/markdown/welcome.mdx";
+// import { Children } from "react";
+
+type childrenProps = {
+  children: React.ReactNode;
+};
+
+function CustomH1({ children }: childrenProps) {
+  return <h1 style={{ color: "blue", fontSize: "100px" }}>{children}</h1>;
+}
+
+const overrideComponents = {
+  h1: CustomH1,
+};
 
 export default function Page() {
-  return <Welcome />;
+  return <Welcome components={overrideComponents} />;
 }
