1995a4045a91534c1ab3da64acc0e2268bd5f17a
aleksejunas <photo@seventyseven.no>
2026-01-06 13:32:26 +0100

    fix: fikset opp i Google Signin + rosa firkant rundt link og knapp
    - fikset Google Signin
    - fikset stygg rosa border rundt knapper og linker

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,6 +1,10 @@
 import { createServerClient } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
 import { cookies } from "next/headers";
+import type { CookieOptions } from "@supabase/ssr";
+
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export async function GET(request: NextRequest) {
   const { searchParams, origin } = new URL(request.url);
@@ -8,19 +12,32 @@ export async function GET(request: NextRequest) {
   // if "next" is in param, use it as the redirect URL
   const next = searchParams.get("next") ?? "/";
 
-  if (code) {
-    const cookieStore = await cookies();
-    const supabase = createServerClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-      { cookies: { ...cookieStore } },
-    );
-    const { error } = await supabase.auth.exchangeCodeForSession(code);
-    if (!error) {
-      return NextResponse.redirect(new URL(next, origin));
-    }
+  if (!code) {
+    return NextResponse.redirect(`${origin}/login?error=missing_code`);
   }
+  const cookieStore = await cookies();
 
+  const supabase = createServerClient(url, key, {
+    // process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    // process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+
+    cookies: {
+      get(name: string) {
+        return cookieStore.get(name)?.value;
+      },
+      set(name: string, value: string, options: CookieOptions) {
+        cookieStore.set({ name, value, ...options });
+      },
+      remove(name: string, options: CookieOptions) {
+        cookieStore.set({ name, value: "", ...options, maxAge: 0 });
+      },
+    },
+  });
+
+  const { error } = await supabase.auth.exchangeCodeForSession(code);
+  if (error) {
+    return NextResponse.redirect(`${origin}/login?error=oauth_callback_failed`);
+  }
   // return the user to an error page with instructions
-  return NextResponse.redirect(new URL("/auth/auth-code-error", origin));
+  return NextResponse.redirect(`${origin}${next}`);
 }
